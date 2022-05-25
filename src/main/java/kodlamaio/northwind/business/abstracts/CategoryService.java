c8070f3e085d85b917388a58b7b236a00192a76d
mervegulkansak <merve_kansak@hotmail.com>
2022-05-25 10:01:54 +0300

    25 05 22 commit1

---
+++ src/main/java/kodlamaio/northwind/business/abstracts/CategoryService.java
@@ -0,0 +1,16 @@
+package kodlamaio.northwind.business.abstracts;
+
+
+import java.util.List;
+
+
+
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.concretes.Category;
+
+public interface CategoryService {
+	
+	DataResult<List<Category>> getAll();
+	
+}
